44bb6f9144bb485d62f61f87f22b51b4971e7dad
Ian Jones <ianjosephjones@gmail.com>
2025-08-12 13:02:56 -0400

    Initialize DatingApp API project with basic structure and configurations

---
+++ API/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using API.Entities;
+
+namespace API.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<AppUser> Users { get; set; }
+}
