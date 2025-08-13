641f9210d056115855dcd0b115b5e58146866762
Ian Jones <ianjosephjones@gmail.com>
2025-08-12 21:47:21 -0400

    feat: integrate Tailwind CSS and DaisyUI for styling
    - Added Tailwind CSS and DaisyUI to package.json dependencies.
    - Updated app.html to use Tailwind CSS classes for styling the main card and member list.
    - Refactored app.ts to fetch members using async/await and RxJS's lastValueFrom.
    - Updated styles.css to import Tailwind CSS and DaisyUI.
    - Created postcss configuration file for Tailwind CSS.
    - Added user avatar image to the public directory.

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, resource, signal } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,13 +11,18 @@ import { Component, inject, OnInit, resource, signal } from '@angular/core';
 export class App implements OnInit {
   private http = inject(HttpClient);
   protected title = 'Dating App';
+  protected members = signal<any>([]);
 
-  ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/members').subscribe({
-      next: resource => console.log(resource),
-      error: error => console.error(error),
-      complete: () => console.log('Request complete')
-    });
+  async ngOnInit() {
+    this.members.set(await this.getMembers());
   }
 
+  async getMembers() {
+    try {
+      return lastValueFrom(this.http.get('https://localhost:5001/api/members'));
+    } catch (error) {
+      console.error(error);
+      return []; 
+    }
+  }
 }
